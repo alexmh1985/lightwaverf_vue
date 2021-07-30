ad640fdb8dd024181335ebb513ca873a446120d2
alex-home <alex@stairbox.com>
2021-07-30 07:01:16 +0100

    split into 2 b-cols

--- src/components/devices.vue
+++ src/components/devices.vue
@@ -1,18 +1,22 @@
 <template>
   <b-container fluid>
     <div v-if="rooms">
-      <b-card v-for="room, room_name in rooms" :key="room_name" :header="room_name" class="mb-4" header-bg-variant="secondary" header-text-variant="white">
-        <b-row>
-          <b-col cols="2" v-for="items, item_name in room" :key="item_name" class="mb-2">
-            <b-card :header="item_name" border-variant="primary" header-bg-variant="light">
-              <div v-for="feature, feature_name in items" :key="feature_name">
-                <b-btn v-if="feature.switch" @click="controlDevice(feature.switch.id, 'switch', feature.switch.status), feature.switch.status = feature.switch.status === 1 ? 0 : 1" class="mb-2" :variant="feature.switch.status === 1 ? 'secondary' : 'success'" size="sm">{{feature_name}}</b-btn>
-                <b-form-input v-if="feature.dimLevel && feature.switch && feature.switch.status === 1" type="range" @change="controlDevice(feature.dimLevel.id, 'dim', feature.dimLevel.status)" v-model="feature.dimLevel.status" min="0" max="100"></b-form-input>
-              </div>
-            </b-card>
-          </b-col>
-        </b-row>
-      </b-card>
+      <b-row>
+        <b-col cols="6 " v-for="room, room_name in rooms" :key="room_name">
+          <b-card :header="room_name" class="mb-4" header-bg-variant="secondary" header-text-variant="white">
+            <b-row>
+              <b-col cols="2" v-for="items, item_name in room" :key="item_name" class="mb-2">
+                <b-card :header="item_name" border-variant="primary" header-bg-variant="light">
+                  <div v-for="feature, feature_name in items" :key="feature_name">
+                    <b-btn v-if="feature.switch" @click="controlDevice(feature.switch.id, 'switch', feature.switch.status), feature.switch.status = feature.switch.status === 1 ? 0 : 1" class="mb-2" :variant="feature.switch.status === 1 ? 'secondary' : 'success'" size="sm">{{feature_name}}</b-btn>
+                    <b-form-input v-if="feature.dimLevel && feature.switch && feature.switch.status === 1" type="range" @change="controlDevice(feature.dimLevel.id, 'dim', feature.dimLevel.status)" v-model="feature.dimLevel.status" min="0" max="100"></b-form-input>
+                  </div>
+                </b-card>
+              </b-col>
+            </b-row>
+          </b-card>
+        </b-col>
+      </b-row>
     </div>
     <div v-else>
       No response from api
